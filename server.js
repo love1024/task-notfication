01ae5ece559fbf390189c38d460752c73b6731ff
Lovepreet Singh <lovepreet.singh@nagarro.com>
2018-05-18 14:55:44 +0530

    Test'

--- server.js
+++ server.js
@@ -89,8 +89,26 @@ app.post('/db/tasks', (req, res, next) => {
 
 app.post('/db/subscribe', (req, res, next) => {
   console.log(req.body);
+
+  const notificationPayload = {
+    "notification": {
+      "title": "Angular News",
+      "body": "Newsletter Available!",
+      "icon": "assets/main-page-logo-small-hat.png",
+      "vibrate": [100, 50, 100],
+      "data": {
+        "dateOfArrival": Date.now(),
+        "primaryKey": 1
+      },
+      "actions": [{
+        "action": "explore",
+        "title": "Go to the site"
+      }]
+    }
+  };
+
   webpush.sendNotification(
-    req.body, JSON.stringify({ sample: "SAMPLE TEST" }))
+    req.body, JSON.stringify(notificationPayload))
     .then(() => res.status(200).json({ message: 'sent successfully.' }))
     .catch(err => {
       console.error("Error sending notification, reason: ", err);
